04b20c07853c3dc2f8a60400031ad38e32fb7d19
AnonymousBeetle <108432570+AnonymousBeetle@users.noreply.github.com>
2023-06-19 19:40:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -33,7 +33,19 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
-        
+        console.log(results);
+        ScoreRightWrist = results[0].pose.keypoints[10].score;
+        ScoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("ScoreRightWrist = " + ScoreRightWrist + "ScoreLeftWrist = " + ScoreLeftWrist);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX + "rightWristY ="+ rightWristY)
     }
 }
 
